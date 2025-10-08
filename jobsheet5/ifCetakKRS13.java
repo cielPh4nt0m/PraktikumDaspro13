65b4b954c49ec056ad9ed3d7f4667632332f23d0
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2025-10-08 09:03:32 +0700

    percobaan 1

--- jobsheet5/ifCetakKRS13.java
+++ jobsheet5/ifCetakKRS13.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class ifCetakKRS13 {
-    public static void main(String[] args) {
+
+    static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS SIAKAD ---");
@@ -11,6 +12,9 @@ public class ifCetakKRS13 {
         if (uktLunas) { //Gunakan nama yang sama persis
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA ");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+        
         }
 
          input.close(); // opsional, untuk menutup Scanner
